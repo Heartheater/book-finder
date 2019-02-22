cf0359658843081a7ec51258710e31315f9bd3ad
cinder <meikopolis@gmail.com>
2019-02-22 16:51:59 -0700

    added static HTML

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ class App extends Component {
   render() {
     return (
       <main id="app">
+        <h1 id="app-title">Book Finder</h1>
+        <div className="search-bar-container">
+          <div className="search-bar">
+            <input className="search-input" type="text"></input>
+            <button className="delete-search">x</button>
+          </div>
+          <button className="submit-search" type="submit">submit</button>
+        </div>
+
+        <section className="results-container">
+        
+          <div className="book">
+            <img src="https://images.penguinrandomhouse.com/cover/9780375823459" alt="golden-compass-cover" className="book-cover"/>
+            <h4 className="book-title">The Golden Compass</h4>
+            <p className="book-author">By Philip Pullman</p>
+            <button className="book-info-btn">View more</button>
+          </div>
+        </section>
       </main>
     );
   }
